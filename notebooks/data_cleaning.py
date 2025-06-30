a00deacc158d71af73f0d8b5b8d0970ef779a3cf
Akshay Nasanakota <akshaynasanakota003@gmail.com>
2025-06-30 18:00:47 +0530

    Removed bugs from data_cleaning.py

--- notebooks/data_cleaning.py
+++ notebooks/data_cleaning.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('C:/Users/aksha/Exoplanet_Habitability_Index_Predictor/datasets/exoplanet_dataset.csv')
+df = pd.read_csv("C:/Users/aksha/OneDrive/Desktop/Akshay/Projects/Hackathon Projects/Exoplanet_Habitability_Index_Predictor/datasets/exoplanet_dataset.csv")
 df.head()
 
-print(df.isnull().sum())
 #drop unnecessary columns
 drop_cols = ['P_DISCOVERY_FACILITY', 'P_YEAR', 'P_UPDATE', 'P_OMEGA', 'S_NAME_HD', 'S_NAME_HIP', 'S_RA', 
              'S_DEC', 'S_RA_STR', 'S_DEC_STR', 'P_INCLINATION', 'S_DISTANCE', 'P_DISTANCE', 'S_RA_TEXT', 
@@ -16,7 +15,7 @@ median_impute_cols = ['P_MASS', 'P_RADIUS', 'P_PERIOD', 'P_SEMI_MAJOR_AXIS', 'P_
                       'S_TEMPERATURE', 'S_MASS', 'S_RADIUS', 'S_METALLICITY', 'S_LOG_LUM', 'S_LOG_G',
                       'P_ESCAPE', 'P_POTENTIAL', 'P_GRAVITY', 'P_DENSITY', 'P_HILL_SPHERE', 'P_DISTANCE', 
                       'P_PERIASTRON', 'P_APASTRON', 'P_DISTANCE_EFF', 'P_FLUX', 'P_TEMP_EQUIL', 
-                      'S_LUMINOSITY', 'S_SNOW_LINE', 'S_ABIO_ZONE', 'S_AGE', 'P_TEMP_SURF']
+                      'S_LUMINOSITY', 'S_SNOW_LINE', 'S_ABIO_ZONE', 'S_TIDAL_LOCK', 'S_AGE', 'P_TEMP_SURF']
 
 for cols in median_impute_cols:
     if cols in df.columns:
@@ -37,8 +36,12 @@ le = LabelEncoder()
 for col in categorical_data:
     df[col] = le.fit_transform(df[col])
 
-temp_map = {0: 'Cold', 1: 'Warm', 2: 'Hot'}
-df['P_TYPE_TEMP'] = df['P_TYPE_TEMP'].map(temp_map)
+from sklearn.preprocessing import OrdinalEncoder
+
+ord_data = [['Cold', 'Warm', 'Hot']]
+oe = OrdinalEncoder(categories= ord_data)
+oe.fit(df[['P_TYPE_TEMP']])
+df['P_TYPE_TEMP'] = oe.transform(df[['P_TYPE_TEMP']])
 
 #Removing outliers using the IQR method
 outliers = ['P_MASS', 'P_RADIUS', 'P_PERIOD', 'P_SEMI_MAJOR_AXIS', 'P_ECCENTRICITY', 'S_MAG', 'S_TEMPERATURE',
@@ -46,17 +49,17 @@ outliers = ['P_MASS', 'P_RADIUS', 'P_PERIOD', 'P_SEMI_MAJOR_AXIS', 'P_ECCENTRICI
             'P_GRAVITY', 'P_HILL_SPHERE', 'P_PERIASTRON', 'P_APASTRON', 'P_FLUX', 'P_TEMP_SURF', 'S_LUMINOSITY',
             'S_ABIO_ZONE', 'S_TIDAL_LOCK']
 
+mask = pd.Series(True, index=df.index)
+
 for col in outliers:
     Q1 = df[col].quantile(0.25)
     Q3 = df[col].quantile(0.75)
-
     IQR = Q3 - Q1
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+    mask &= df[col].between(lower, upper)
 
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-
-    df = df[(df[col] >= lower_bound) & (df[col] <= upper_bound)]
-
+df = df[mask]
 
 from sklearn.preprocessing import MinMaxScaler
 
@@ -66,13 +69,11 @@ col_names = ['P_MASS', 'P_RADIUS', 'P_PERIOD', 'P_SEMI_MAJOR_AXIS', 'P_ECCENTRIC
              'S_MASS', 'S_RADIUS', 'S_METALLICITY', 'S_AGE', 'S_LOG_LUM', 'S_LOG_G', 'P_ESCAPE', 'P_POTENTIAL', 
              'P_GRAVITY', 'P_DENSITY', 'P_HILL_SPHERE', 'P_PERIASTRON', 'P_APASTRON', 'P_DISTANCE_EFF', 
              'P_FLUX', 'P_TEMP_EQUIL', 'P_TEMP_SURF', 'S_TYPE_TEMP', 'S_LUMINOSITY', 'S_SNOW_LINE', 
-             'S_ABIO_ZONE', 'S_TIDAL_LOCK', 'P_HABZONE_OPT', 'P_HABZONE_CON', 'P_TYPE_TEMP']
+             'S_ABIO_ZONE', 'S_TIDAL_LOCK', 'P_HABZONE_OPT', 'P_HABZONE_CON']
 
 for col in col_names:
-    scaler.fit(df[[col]])
-    df[col] = pd.DataFrame(scaler.transform(df[[col]]))
+    df[col] = scaler.fit_transform(df[[col]]).ravel()
 
 df.drop_duplicates(inplace=True)
 
-df.to_csv('C:/Users/aksha/Exoplanet_Habitability_Index_Predictor/datasets/exoplanet_dataset_cleaned.csv', index=False)
-print(df.describe())
+df.to_csv('C:/Users/aksha/OneDrive/Desktop/Akshay/Projects/Hackathon Projects/Exoplanet_Habitability_Index_Predictor/datasets/exoplanet_dataset_cleaned.csv', index=False)
